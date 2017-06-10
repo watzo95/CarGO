786e267672b86e9a7dce47abc843b6b09aefdedf
watzo95 <marko.watzak@hotmail.com>
2017-06-11 01:19:18 +0200

    Dodano: Google Auth s FireBase - 11.6.2017

---
+++ app/src/main/java/com/example/marko/cargo/BaseActivity.java
@@ -0,0 +1,38 @@
+package com.example.marko.cargo;
+
+/**
+ * Created by marko on 11. 06. 2017.
+ */
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
